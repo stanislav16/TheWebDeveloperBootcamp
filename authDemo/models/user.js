154ae53cbd65d33c53c0e79eb3b48c9a79bf1009
stanislav16 <sormanliev@gmail.com>
2024-02-22 14:10:08 +0200

    More authentication

--- authDemo/models/user.js
+++ authDemo/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -9,6 +10,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({ username });
+  const isValid = await bcrypt.compare(password, foundUser.password);
+  if (isValid) {
+    return foundUser;
+  } else {
+    return false;
+  }
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hashSync(this.password, 12);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
